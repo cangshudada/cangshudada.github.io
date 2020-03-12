2d3a052774124d7831f7aa370095df66fb40e4ca
cangshudada <15342240569@163.com>
2020-03-12 09:38:11 +0800

    完成banner部分

---
+++ precache-manifest.f71416f4d8410dd8b8eef48b35edf378.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ec3cdcd1c153cee59da5062fdc412f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d2736d65494c9bc06b2b",
+    "url": "/static/css/2.8a75941f.chunk.css"
+  },
+  {
+    "revision": "62f5c28c1d935905b2cf",
+    "url": "/static/css/main.60f40899.chunk.css"
+  },
+  {
+    "revision": "d2736d65494c9bc06b2b",
+    "url": "/static/js/2.888e8816.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/static/js/2.888e8816.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f5c28c1d935905b2cf",
+    "url": "/static/js/main.e0263fbc.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  },
+  {
+    "revision": "3b4f58f903de058ed46ad45b0334049c",
+    "url": "/static/media/arrowLeft.3b4f58f9.svg"
+  },
+  {
+    "revision": "e0ed71a99554332bdfd067c63ca3f211",
+    "url": "/static/media/banner.e0ed71a9.svg"
+  },
+  {
+    "revision": "c1ee40ac829b96ebf139dbf7f4b73e00",
+    "url": "/static/media/demian.c1ee40ac.jpg"
+  },
+  {
+    "revision": "aad30dc1c1bee4211ffbc47782c83633",
+    "url": "/static/media/email.aad30dc1.svg"
+  },
+  {
+    "revision": "8875c8ea1caf9ae43dba04d108844fe7",
+    "url": "/static/media/github.8875c8ea.svg"
+  },
+  {
+    "revision": "2a58207f0d94d423f926b2185fe7e8f5",
+    "url": "/static/media/logo.2a58207f.svg"
+  },
+  {
+    "revision": "cf8ca593d3a48aa549972ffe0c198a0f",
+    "url": "/static/media/mobile.cf8ca593.svg"
+  },
+  {
+    "revision": "72e0485056069ea1226038f39cb40e30",
+    "url": "/static/media/qq.72e04850.svg"
+  },
+  {
+    "revision": "b0cdd1ae90d3bf1bc308969e8705903f",
+    "url": "/static/media/wechat.b0cdd1ae.svg"
+  }
+]);
